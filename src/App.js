37d4e3411d53a780f2b79f1207dda2cc0c7380fe
osezei <john.osezei@yahoo.com>
2023-02-09 21:15:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import ProductsPage from "./pages/ProductsPage";
+import SingleProductPage from "./pages/SingleProductPage";
+import Error from "./pages/Error";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/products" element={<ProductsPage />} />
+        <Route exact path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>home</h2>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Home;
