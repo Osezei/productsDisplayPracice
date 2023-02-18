a181cc0e7caf67b8fa2a93451b912fe0f6ec9444
osezei <john.osezei@yahoo.com>
2023-02-18 21:47:21 +0100

    fix homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,28 @@ import styled from "styled-components";
 
 const Header = () => {
   return (
-    <Main className=" flex justify-between bg-green-700 font-normal text-sm text-white">
-      <div className="lg:flex justify-between sm:hidden">
+    <Main className="sm:hidden md:hidden lg:flex justify-between bg-green-700 font-normal text-sm text-white">
+      <div className="flex justify-between">
         <span className="pr-1 icon-arrangement">
           <BsTelephone />
         </span>
         <p>08055223390</p>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between ml-auto mr-auto">
         <p className="pr-3 ">Get 50% Off on Selected Items</p> |{" "}
         <p className="pl-3">Shop Now</p>
       </div>
       <div className="flex justify-between">
-        <div className="flex justify-between">
-          <p className="pr-1">Eng</p>
-          <span className="icon-arrangement pr-1">
-            <BsArrowDownShort />
-          </span>
-        </div>
-        <div className="flex justify-between">
-          <p className="pr-1">Location </p>
-          <span className="icon-arrangement">
-            <BsArrowDownShort />
-          </span>
-        </div>
+        <p className="pr-1">Eng</p>
+        <span className="icon-arrangement pr-1">
+          <BsArrowDownShort />
+        </span>
+      </div>
+      <div className="flex justify-between">
+        <p className="pr-1">Location </p>
+        <span className="icon-arrangement">
+          <BsArrowDownShort />
+        </span>
       </div>
     </Main>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,21 +5,25 @@ import hero3 from "../Images/unsplash.jpg";
 
 const Hero = () => {
   return (
-    <Main className="container text-transform: capitalize">
-      <img src={hero3} alt="my-hero" className="image relative" />
+    <Main className="container text-transform: capitalize static">
+      <section className="relative">
+        <img src={hero3} alt="my-hero" className="image" />
 
-      <div className=" text-5xl text-white font-bold absolute">
-        <p className=" text-white">
-          grab upto 50% off on <br />
-          selected kicks
-        </p>
-        <Link
-          to="/products"
-          className="text-white bg-green-700 p-2 pr-4 pl-4 rounded-full text-xl font-medium text-transform: capitalize"
-        >
-          buy now
-        </Link>
-      </div>
+        <div className="sm:text-3xl md:text-5xl text-white font-bold">
+          <div className="absolute bottom-40 left-3">
+            <p className=" text-white">
+              grab upto 50% off on <br />
+              selected kicks
+            </p>
+          </div>
+          <Link
+            to="/products"
+            className="absolute bottom-28 left-3 text-white sm:text-sm bg-green-700 p-2 pr-4 pl-4 rounded-full font-medium text-transform: capitalize lg:text-xl"
+          >
+            buy now
+          </Link>
+        </div>
+      </section>
     </Main>
   );
 };
